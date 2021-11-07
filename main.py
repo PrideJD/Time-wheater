a93921851a5def5aefe2903d964189379d3b6743
PrideJD <luftsl@yandex.ru>
2021-11-07 23:43:30 +0300

    realise v1

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, url_for
+from datetime import datetime, timedelta
+import requests, datetime
+
+
+app = Flask(__name__)
+
+@app.route('/wheater')
+def get_weather():
+    params = {"access_key": "7232fd01ee4fdc36a40fb942687c8dcc", "query": "Moscow"}
+    api_result = requests.get('http://api.weatherstack.com/current', params)
+    api_response = api_result.json()
+    return f"Сейчас в Москве {api_response['current']['temperature']} градусов"
+
+@app.route('/time')
+def sabe():
+    dtmn = datetime.datetime.now()
+    return str(dtmn)
+
+
+if __name__ == '__main__':
+    app.run()
